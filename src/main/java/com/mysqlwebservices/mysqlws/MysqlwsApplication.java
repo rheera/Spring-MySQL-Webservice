921b1978938a2e83c429687e830db0740fb9d6e0
Raman Heera <rheera2@uwo.ca>
2019-04-21 13:03:29 -0400

    Initial commit

---
+++ src/main/java/com/mysqlwebservices/mysqlws/MysqlwsApplication.java
@@ -0,0 +1,13 @@
+package com.mysqlwebservices.mysqlws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysqlwsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MysqlwsApplication.class, args);
+    }
+
+}
